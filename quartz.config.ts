2c7e99c37038aef9b33c7d7707ac810cd5bc149f
lichtobergo <62296530+lichtobergo@users.noreply.github.com>
2024-05-21 11:50:14 +0200

    Quartz sync: May 21, 2024, 11:50 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: " Zeit der Wölfe",
+    pageTitle: "Zeit der Wölfe",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
